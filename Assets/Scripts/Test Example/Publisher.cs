e57f7240f8b24448cfabf0993a18d66b058f57c6
new <newsoft@outlook.com>
2023-06-27 09:25:09 +0330

    Fix some comments

--- Assets/Scripts/Test Example/Publisher.cs
+++ Assets/Scripts/Test Example/Publisher.cs
@@ -39,49 +39,49 @@ public class Publisher : MonoBehaviour
     //--------- Send Message to the target GameObject
     private void RunMessages()
     {
-        // send to GameObject
+        // send message to the GameObject
         MessageManager.Instance.SendMessage(_target, "Damage", 10);
 
-        // send to specified component on target gameObject
+        // send message to specified component on the target gameObject
         MessageManager.Instance.SendMessage(_target.GetComponent<TargetAgent>(), "Damage", 20);
 
-        // send to GameObject childiins
+        // send message to the GameObject children's
         MessageManager.Instance.SendMessageToChildren(_target, "Damage", 30);
 
-        // send to GameObjects - Upwards
+        // send message to the GameObjects - Upwards
         MessageManager.Instance.SendMessageUpwards(_target, "Damage", 40);
 
-        // broadcast to all GameObjects in the scene
+        // broadcast message to all GameObjects in the scene
         MessageManager.Instance.BroadCastMessage("Damage");
 
-        // broadcast to all GameObjects in the scene - Generic version (int) - automaticlly detect proper type as data value
+        // broadcast message to all GameObjects in the scene - Generic version (int) - automaticlly detect proper type as data value
         MessageManager.Instance.BroadCastMessage("Damage", 1000);
 
-        // broadcast to all GameObjects in the scene - Generic version (float) - automaticlly detect proper type as data value
+        // broadcast message to all GameObjects in the scene - Generic version (float) - automaticlly detect proper type as data value
         MessageManager.Instance.BroadCastMessage("Damage", 2000.5f);
 
-        // broadcast to all GameObjects in the scene - Generic version (string) - - automaticlly detect proper type as data value
+        // broadcast message to all GameObjects in the scene - Generic version (string) - - automaticlly detect proper type as data value
         MessageManager.Instance.BroadCastMessage("Damage", " do damage");
 
-        // send to Coroutine with specified component on target gameObject - no param 
+        // send message to Coroutine with specified component on the target gameObject - no param 
         MessageManager.Instance.StartCoroutine(_target.GetComponent<TargetAgent>(), "MyCoroutin");
 
-        // send to Coroutine with specified component on target gameObject - 1 param 
+        // send message to Coroutine with specified component on the target gameObject - 1 param 
         MessageManager.Instance.StartCoroutine(_target.GetComponent<TargetAgent>(), "MyCoroutin", 5);
 
-        // send message to Coroutine no param 
+        // send message to Coroutine on the target gameObject - no param 
         MessageManager.Instance.StartCoroutine(_target, "MyCoroutin");
 
-        // send message to Coroutine on param
+        // send message to Coroutine on the target gameObject - 1 param
         MessageManager.Instance.StartCoroutine(_target, "MyCoroutin", 255);
 
-        // stop Coroutine
+        // stop Coroutine on the target gameObject
         MessageManager.Instance.StopCoroutine(_target, "MyCoroutin");
 
-        // send message to Coroutine no param - send to a gameobject component
+        // send message to Coroutine no param - send to the gameobject component
         MessageManager.Instance.StartCoroutine(_target.transform, "MyCoroutin");
 
-        // send message to Coroutine one param - send to a gameobject component
+        // send message to Coroutine 1 param - send to the gameobject component
         MessageManager.Instance.StartCoroutine(_target.transform, "MyCoroutin", "Run Coroutine");
     }
 }
